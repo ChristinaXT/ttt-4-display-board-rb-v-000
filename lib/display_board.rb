12352da6b123f2163b239d170d138a3afee86c71
Christina Williams <christinaxtwilliams@gmail.com>
2018-07-26 19:46:22 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,8 +3,8 @@ separate = "|"
 lines = "-----------"
   puts "#{board[0]} #{separate} #{board[1]} #{separate} #{board[2]}"
   puts "#{lines}"
-  puts "#{cell}#{separate}#{cell}#{separate}#{cell}"
+  puts "#{board[3]} #{separate} #{board[4]} #{separate} #{board[5]}"
   puts "#{lines}"
-  puts "#{cell}#{separate}#{cell}#{separate}#{cell}"
+  puts "#{board[6]} #{separate} #{board[7]} #{separate} #{board[8]}"
 end 
 
