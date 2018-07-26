33069df005857eccaa9bf6e462b5f9dfc62b2dc1
Christina Williams <christinaxtwilliams@gmail.com>
2018-07-26 19:44:40 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 def display_board (board)
 separate = "|"
 lines = "-----------"
-  puts "#{board[0]}#{separate}#{cell}#{separate}#{cell}"
+  puts "#{board[0]} #{separate} #{board[1]} #{separate} #{board[2]}"
   puts "#{lines}"
   puts "#{cell}#{separate}#{cell}#{separate}#{cell}"
   puts "#{lines}"
